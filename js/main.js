a7256f6f66cc1be29738be958786413beab80e04
janteuni <janteuni@student.42.fr>
2014-08-11 14:37:43 +0200

    add 4 color changes

--- js/main.js
+++ js/main.js
@@ -2,19 +2,34 @@ function changeColor(color) {
 	console.log(color);
 	var green = [
 		"#26a69a",
-		"#0097a7",
-		"#80cbc4"
+		"#42bd41",
+		"#0097a7"
 	];
 	var blue = [
 		"#5677fc",
 		"#03a9f4",
 		"#0097a7"
 	];
+	
+	var red = [
+		"#5e35b1",
+		"#7986cb",
+		"#ad1457"
+	];
 
+	var violet = [
+		"#B50100",
+		"#0451DE",
+		"#B50100"
+	];
 	if (color == 'green') {
 		color = green;
 	} else if (color == 'blue') {
 		color = blue;
+	} else if (color == 'red') {
+		color = red;
+	} else {
+		color = violet;
 	}
 	document.getElementById("w1").setAttribute("fill", color[0]);
 	document.getElementById("w2").setAttribute("fill", color[1]);
