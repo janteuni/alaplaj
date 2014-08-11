7462adad94d19ce9fddd495f95b0a2e3fbb52bc0
janteuni <janteuni@student.42.fr>
2014-08-11 14:28:04 +0200

    add changing color with js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-	var i;
-	console.log('toto');
-	i = 1;
-	while (i < 100) {
-		$(".tv").prepend('<div class="pix' + i + '"></div>');
-		i++;
+function changeColor(color) {
+	console.log(color);
+	var green = [
+		"#26a69a",
+		"#0097a7",
+		"#80cbc4"
+	];
+	var blue = [
+		"#5677fc",
+		"#03a9f4",
+		"#0097a7"
+	];
+
+	if (color == 'green') {
+		color = green;
+	} else if (color == 'blue') {
+		color = blue;
 	}
+	document.getElementById("w1").setAttribute("fill", color[0]);
+	document.getElementById("w2").setAttribute("fill", color[1]);
+	document.getElementById("w3").setAttribute("fill", color[2]);
+}
